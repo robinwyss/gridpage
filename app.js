cb539cf87e4ed167322491a5917b1ab6886d66c9
Robin Wyss <rowyss@gmail.com>
2017-04-30 12:11:48 -0400

    improved UI, added options to edit and delete

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ $(function($) {
     };
 
     var resizeWidget = function(widget) {
+      // adjust size of p-element for images
+      widget.find('img').each(function(){
+        var width = $(this).outerWidth( true );
+        $(this).closest('p').width(width);
+      })
       var widgetContent = widget.find('.grid-stack-item-content div');
       var contentWidth = widgetContent.width();
       var contentHeight = widgetContent.height();
@@ -44,7 +49,7 @@ $(function($) {
 
     var removeWidget = function(widget) {
       gridStack.removeWidget(widget);
-    }
+    };
 
     var save = function() {
       var items = _.map($('.grid-stack .grid-stack-item:visible'), function(el) {
@@ -117,21 +122,47 @@ $(function($) {
           content: content
         });
         gridManager.resizeWidget(widget);
+        reloadEditOptions();
       };
       createEditor(saveAction);
     };
 
-    var editWidget = function(element) {
-      var content = element.find('.grid-stack-item-content div').html();
+    var editWidget = function(widget) {
+      var content = widget.find('.grid-stack-item-content div').html();
       var saveAction = function(content) {
-        element.find('.grid-stack-item-content div').html(content);
+        widget.find('.grid-stack-item-content div').html(content);
+        reloadEditOptions();
+        gridManager.resizeWidget(widget);
       };
       createEditor(saveAction, content);
     };
 
+    var reloadEditOptions = function() {
+      hideEditOptions();
+      showEditOptions();
+    };
+
+    var showEditOptions = function() {
+      $('.grid-stack-item-content').append('<div class="grid-element-edit"><div><button class="btn edit">edit</button><button class="btn delete">delete</button></div></div>');
+      $('.grid-stack-item-content').find('button.edit').click(function(evt) {
+        var widget = $(evt.target).closest('.grid-stack-item');
+        editor.editWidget(widget);
+      });
+      $('.grid-stack-item-content').find('button.delete').click(function(evt) {
+        var widget = $(evt.target).closest('.grid-stack-item');
+        gridManager.removeWidget(widget);
+      });
+    };
+
+    var hideEditOptions = function() {
+      $('.grid-stack-item-content .grid-element-edit').remove();
+    };
+
     return {
       createWidget: createWidget,
-      editWidget: editWidget
+      editWidget: editWidget,
+      showEditOptions: showEditOptions,
+      hideEditOptions: hideEditOptions
     };
   })(gridManager);
 
@@ -139,15 +170,9 @@ $(function($) {
   $('#add-new-widget').click(editor.createWidget);
 
   $('#save').click(gridManager.save);
-  gridManager.load();
 
-  $('.grid-stack-item-content').append('<div class="grid-element-edit"><div><button class="btn edit">edit</button><button class="btn delete">delete</button></div></div>');
-  $('.grid-stack-item-content').find('button.edit').click(function(evt) {
-    var widget = $(evt.target).closest('.grid-stack-item');
-    editor.editWidget(widget);
-  });
-  $('.grid-stack-item-content').find('button.delete').click(function(evt) {
-    var widget = $(evt.target).closest('.grid-stack-item');
-    gridManager.removeWidget(widget);
-  });
+  gridManager.load();
+  editor.showEditOptions();
+
+
 });
